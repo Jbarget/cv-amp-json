8886aeb3b972e872dce44d6a28561276b0b52580
jbarget <dear.justen@gmail.com>
2018-10-19 15:06:38 +0200

    adding pdf download option

--- README.md
+++ README.md
@@ -6,7 +6,13 @@ Getting Started
 
 1. Clone the repo: `git clone`
 1. Move into the cv-amp directory: `cd cv-amp`
-1. Edit the values in`cv.json` to your info
+1. Edit the values in `cv.json` to your info
 1. Run `node index.js`
 1. You should now have a `index.html` in the root of the directory :tada::tada::tada:
 1. Star the repo :star:
+
+If you want to download your CV as a PDF then run `node index.js --pdf` which produces `cv.pdf` file.
+
+## Problems
+
+At the moment we're using `puppeteer` to create the PDF. This downloads Chromium (86 MB) - not good. We'll need to switch to an alternative!
--- index.js
+++ index.js
@@ -1,9 +1,53 @@
+// !/usr/bin/env node
+
 const fs = require("fs");
+const path = require("path");
 const handlebars = require("handlebars");
+const puppeteer = require("puppeteer");
 
 const context = fs.readFileSync("cv.json").toString("utf-8");
-const source = fs.readFileSync("cv.hbs").toString();
-var template = handlebars.compile(source);
+const isPDF = process.argv[2] === "--pdf";
+const source = isPDF
+  ? console.log("PDF version") || fs.readFileSync("cv-pdf.hbs").toString()
+  : console.log("HTML version") || fs.readFileSync("cv.hbs").toString();
 
-var html = template(JSON.parse(context));
-fs.writeFileSync("index.html", html);
+const template = handlebars.compile(source);
+
+const html = template(JSON.parse(context));
+
+if (!isPDF) fs.writeFileSync("index.html", html);
+else {
+  (async () => {
+    const options = {
+      format: "A4",
+      margin: {
+        bottom: "20px"
+      },
+      displayHeaderFooter: false,
+      printBackground: true,
+      path: "cv.pdf"
+    };
+    const browser = await puppeteer.launch({
+      args: ["--no-sandbox"],
+      headless: true
+    });
+    const page = await browser.newPage();
+    await page.goto(`data:text/html,${html}`, {
+      waitUntil: "networkidle0"
+    });
+    await page.setViewport({
+      width: 1268,
+      height: 800
+    });
+    await page.pdf(options);
+    const dimensions = await page.evaluate(() => {
+      return {
+        width: document.documentElement.clientWidth,
+        height: document.documentElement.clientHeight,
+        deviceScaleFactor: window.devicePixelRatio
+      };
+    });
+
+    await browser.close();
+  })();
+}
