8dc63739affd846d42bcb30af47cfd4b33cf1e1e
jbarget <dear.justen@gmail.com>
2021-06-20 09:37:30 +0100

    removes hapi from skills

--- index.js
+++ index.js
@@ -1,36 +1,35 @@
 // !/usr/bin/env node
 
-const fs = require("fs");
-const handlebars = require("handlebars");
-const puppeteer = require("puppeteer");
+const fs = require('fs');
+const handlebars = require('handlebars');
+const puppeteer = require('puppeteer');
 
-const data = fs.readFileSync("cv.json").toString("utf-8");
-const isPDF = process.argv[2] === "--pdf";
+const data = fs.readFileSync('cv.json').toString('utf-8');
+const isPDF = process.argv[2] === '--pdf';
 const source = isPDF
-  ? console.log("PDF version") || fs.readFileSync("cv-pdf.hbs").toString()
-  : console.log("HTML version") || fs.readFileSync("cv.hbs").toString();
+  ? console.log('PDF version') || fs.readFileSync('cv-pdf.hbs').toString()
+  : console.log('HTML version') || fs.readFileSync('cv.hbs').toString();
 
 const template = handlebars.compile(source);
 const context = JSON.parse(data);
 const html = template(context);
 
-if (!isPDF) fs.writeFileSync("index.html", html);
+if (!isPDF) fs.writeFileSync('index.html', html);
 else {
   (async () => {
     const options = {
-      format: "A4",
+      format: 'A4',
       margin: {
-        bottom: "20px"
+        bottom: '20px',
       },
-
-      path: `${context.basic.first_name}_${context.basic.last_name}.pdf`
+      path: `${context.basic.first_name}_${context.basic.last_name}.pdf`,
     };
     const browser = await puppeteer.launch({
       headless: true,
     });
     const page = await browser.newPage();
     await page.setContent(html, {
-      waitUntil: "networkidle0"
+      waitUntil: 'networkidle0',
     });
 
     await page.pdf(options);
